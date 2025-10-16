adf79c6f9e5aa1aaf7cb7341a8a9c0f5e8b2020a
Guido Günther <agx@sigxcpu.org>
2025-10-16 14:25:51 +0200

    Add dir-locals
    Eases formatting in emacs
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+(
+ (markdown-mode . (
+            (fill-column . 100)
+            (display-fill-column-indicator-column . 100)
+            ))
+ (yaml-mode . (
+            (indent-tabs-mode . nil)
+            ))
+)
